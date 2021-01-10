11a9d64379f39fcda709e067ad913e265f817a0f
Chason Xu <chenxiang_chason@sina.com>
2021-01-10 17:06:26 +0800

    添加windows_VSCode项目

---
+++ Windows/Cmake_VSCode/HelloWorld/build/CMakeFiles/chess.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/0_GitRepository/CppTest/src/main.cpp" "E:/0_GitRepository/CppTest/build/CMakeFiles/chess.dir/src/main.cpp.obj"
+  "E:/0_GitRepository/CppTest/src/utils.cpp" "E:/0_GitRepository/CppTest/build/CMakeFiles/chess.dir/src/utils.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Windows/Cmake_VSCode/HelloWorld/build/CMakeFiles/chess.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../bin/chess.exe"
+  "../bin/chess.exe.manifest"
+  "../bin/chess.pdb"
+  "../bin/libchess.dll.a"
+  "CMakeFiles/chess.dir/src/main.cpp.obj"
+  "CMakeFiles/chess.dir/src/utils.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
